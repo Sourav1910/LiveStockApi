12b090f1a2acdd1d7d90f1627d56353d791082d0
Sourav <baghmar.works@gmail.com>
2020-10-10 23:52:52 +0530

    Port changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const request = require('request-promise')
 const cheerio = require('cheerio')
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 var obj;
 app.get('/', function (req, res) {
     let company_code = req.query.companycCode;
